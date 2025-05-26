e93b1d108ac3700d3b2cd827132dd04a87027fff
saivinaybolloju <saivinay.bolloju@gmail.com>
2025-05-27 01:11:27 +0530

    updatingInfoInCheckout, DynamicRadioButtonChangesMatchwithTitle, and UpdateRadioWithRefreshPage

--- javascript-amazon-project/data/cart.js
+++ javascript-amazon-project/data/cart.js
@@ -4,9 +4,11 @@ if(!cart){
     cart=[{
     productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity:2,
+    deliveryOptionId:'1'
 },{
     productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
     quantity:1,
+    deliveryOptionId:'2'
 }];
 }
 
@@ -26,7 +28,8 @@ export function addToCart(productId){
             }else{
                  cart.push({
                 productId:productId,
-                quantity:1
+                quantity:1,
+                deliveryOptionId:'1'
             });
             }
     saveToLocalStorage();
@@ -41,4 +44,14 @@ export function removeFromCart(productId){
     });
     cart=newCart;
     saveToLocalStorage();
+}
+export function updateDeliveryOption(productId, deliveryOptionId){
+    let matchingItem;
+            cart.forEach((cartItem)=>{
+                if(productId===cartItem.productId){
+                    matchingItem=cartItem;
+                }
+            });
+            matchingItem.deliveryOptionId=deliveryOptionId;
+            saveToLocalStorage();
 }
