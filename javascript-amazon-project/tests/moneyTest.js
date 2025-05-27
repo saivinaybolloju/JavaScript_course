3891bb4fd7628a04d55f3c61426eb2a7a6938f4c
saivinaybolloju <saivinay.bolloju@gmail.com>
2025-05-27 12:51:37 +0530

    Testinginit automaticTesting

---
+++ javascript-amazon-project/tests/moneyTest.js
@@ -0,0 +1,24 @@
+import { formatCurrency } from "../scripts/utils/money.js";
+console.log("Test Suite : formatCurrency")
+
+console.log("converts cents to dollars")
+if(formatCurrency(2095)==='20.95'){
+    console.log("passed");
+}
+else{
+    console.log("failed");
+}
+console.log("Works with 0")
+if(formatCurrency(0)==='0.00'){
+    console.log("passed");
+}
+else{
+    console.log("failed");
+}
+console.log("rounds to nearest Cent")
+if(formatCurrency(2000.5)==='20.01'){
+    console.log("passed");
+}
+else{
+    console.log("failed");
+}
