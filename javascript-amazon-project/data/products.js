3f56ff56dba50c854e26f7e4b30186b536998cc5
saivinaybolloju <saivinay.bolloju@gmail.com>
2025-05-28 23:08:01 +0530

    thisKeywordExamples backendInit

--- javascript-amazon-project/data/products.js
+++ javascript-amazon-project/data/products.js
@@ -47,6 +47,45 @@ class Clothing extends Product{
       `;
    }
 }
+
+// const date= new Date();
+// console.log(date);
+// console.log(date.toLocaleTimeString());
+
+// function logThis(){
+//   console.log(this);
+// }
+// logThis();
+// logThis.call("hello");
+
+// const object3={
+//   method:()=>{
+//     console.log(this);
+//   }
+// }
+// object3.method();
+
+
+export let products=[];
+export function loadProducts(fun){
+  const xhr=new XMLHttpRequest();
+  xhr.addEventListener('load',()=>{
+      products=JSON.parse(xhr.response).map((productDetails)=>{
+  if(productDetails.type==='clothing'){
+    return new Clothing(productDetails);
+  }
+  return new Product(productDetails);
+});
+console.log('load products');
+fun();
+  });
+  xhr.open('GET','https://supersimplebackend.dev/products');
+  xhr.send();
+}
+loadProducts();
+
+
+/*
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -711,4 +750,4 @@ export const products = [
     return new Clothing(productDetails);
   }
   return new Product(productDetails);
-});
+});*/
--- javascript-amazon-project/scripts/checkout.js
+++ javascript-amazon-project/scripts/checkout.js
@@ -1,6 +1,10 @@
+import { loadProducts } from "../data/products.js";
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
+loadProducts(()=>{
+    renderOrderSummary();
+    renderPaymentSummary();
+});
 // import '../data/cart-class.js';
 // import '../data/cart-oop.js';
-renderOrderSummary();
-renderPaymentSummary();
+// import '../data/backend-practice.js';
