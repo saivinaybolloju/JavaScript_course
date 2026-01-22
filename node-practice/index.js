00838a8efb39aa52f41af2d34ff34ba9958338b2
saivinaybolloju <saivinay.bolloju@gmail.com>
2026-01-22 23:58:17 +0530

    nodejs pratice

---
+++ node-practice/index.js
@@ -0,0 +1,43 @@
+const express=require('express')
+
+const app=express();
+app.use(express.json());
+
+const arr=[
+    {id:1,name:'Java'},
+    {id:2,name:'Python'},
+    {id:3,name:'C++'},
+
+]
+
+app.get('/',(req,res)=>{
+    console.log("HELLO WORLD");
+    res.send("HELLO WORLD");
+});
+app.get('/contact/:name',(req,res)=>{
+    console.log("Contact");
+    res.send(req.params.name);
+});
+app.get('/contact/',(req,res)=>{
+    console.log("Contact");
+    res.send(arr);
+});
+app.post('/contact/',(req,res)=>{
+    const {name}=req.body;
+    arr.push({id:arr.length+1,name});
+    res.send(arr);
+});
+app.put('/contact/',(req,res)=>{
+    const {item}=req.body;
+    if (!item) {
+        return res.status(400).send({ message: "Name is required" });
+    }
+    if(arr.find(item=>item.name===req.params.item)){
+        return res.status(400).send("Already there");
+    }
+    arr.push({id:arr.length+1,item});
+    res.send(arr);
+});
+
+
+app.listen(3000);
